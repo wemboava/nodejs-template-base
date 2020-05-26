f788f6320be7e5e6f4d4e94ed81206c08523d063
willian emboava <w.emboava.o@gmail.com>
2020-05-26 16:44:27 -0300

    :sparkles: added eslint and prettier config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express from 'express';
+import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.listen(3333, () => {
-  console.log('🚀 Server started on port :3333');
-})
+app.use(routes);
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-})
+app.listen(3333, () => {
+  console.log("🚀 Server started on port :3333");
+});
